8b250ac02a6d4c1b094e3a9b23a3296e06653652
ALDO LAMBRUSCHINI <adloans@gmail.com>
2015-10-18 21:41:27 -0700

    Aldo Lambruschini

---
+++ MMfunctions.js
@@ -0,0 +1,56 @@
+
+/**
+ * Created by godson on 10/15/15.
+ */
+
+var firstCardClicked = null;
+var secondCardClicked = null;
+var totalPossibleMatches = 9;
+var matchCounter = 0;
+
+var matches = 0;
+var attempts = 0;
+var accuracy = 0;
+var gamesPlayed = 0;
+var displayStats = 0;x
+
+
+function cardClicked(card_id) {
+    $(card_id).addClass("current_card");
+    $(card_id).hide();
+    var frontSource = $(card_id).prev().find("img").attr("src");
+    console.log(frontSource);
+    if (firstCardClicked == null) {
+        firstCardClicked = frontSource;
+        console.log('first click is now front source ' + firstCardClicked);
+        console.log(' click functionality reset ')
+    }
+    else {
+        secondCardClicked = frontSource;
+        console.log('second card clicked is now ' + secondCardClicked);
+        if (secondCardClicked == firstCardClicked) {
+            matchCounter++;
+            firstCardClicked = null;
+            secondCardClicked = null;
+            $(".back").removeClass("current_card");
+            if (matchCounter == totalPossibleMatches) {
+
+                alert("You're Legend!")
+            }
+            else {
+                return console.log('click functionality reset')
+            }
+
+        }
+        else {
+            $(".back").unbind();
+            setTimeout(function () {
+                $(".current_card").show();
+                firstCardClicked = null;
+                secondCardClicked = null;
+                $(".back").removeClass("current_card");
+                console.log('2nd card click done');
+            },200);
+        }
+    }
+}
